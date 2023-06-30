ef53fb18ad0066e48b27360ac5ea99e1baecea41
Gowri-Mohan <118054081+Gowri-Mohan@users.noreply.github.com>
2023-06-30 12:00:07 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,s;
+    printf("enter value for a = \n");
+    scanf("%d",&a);
+    printf("enter value for b = \n");
+    scanf("%d",&b);
+    s=a+b;
+    printf("the sum is %d\n",s);
+
+    return 0;
+}
